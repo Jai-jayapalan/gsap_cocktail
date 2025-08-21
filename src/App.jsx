91ea73289843ce2ef36eda4ec931418a50891040
Jai-jayapalan <jaijayathilak@gmail.com>
2025-08-21 20:28:36 +0530

    completed contact, art

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Hero from './components/Hero'
 import Cocktail from './components/Cocktail'
 import About from './components/About'
 import Art from './components/Art'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -17,6 +19,8 @@ const App = () => {
       <Cocktail />
       <About />
       <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
